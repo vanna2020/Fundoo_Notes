429c4ef40dd7b350cd5ed162fe08386924329007
vanna2020 <vannasingh1999@gmail.com>
2021-12-11 14:20:16 +0530

    Feat: Added ES Lint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    parser: "@babel/eslint-parser",
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true
+    },
+    extends: ["standard"],
+    parserOptions: {
+      ecmaVersion: 13,
+      requireConfigFile: false
+    },
+    rules: {
+      quotes: ["error", "double"],
+      semi: ["error", "always"]
+    }
+  };
