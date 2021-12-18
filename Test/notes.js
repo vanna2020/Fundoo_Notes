4193264bad24f08e5f0d9b1d149ff61191fcbaa0
vanna2020 <vannasingh1999@gmail.com>
2021-12-18 21:03:58 +0530

    Feat: Added testing for Create Note API for CRUD Operations

--- Test/notes.js
+++ Test/notes.js
@@ -2,7 +2,6 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
 const faker = require('faker');
-
 chai.use(chaiHttp);
 const noteDB = require('./notes.json');
 const { expect } = require('chai');
@@ -44,29 +43,30 @@ describe('create notes api', () => {
             });
     });
 });
+
 // get note test cases
 describe('get notes api', () => {
     it('notes', (done) => {
-      const token = noteDB.notes.getNoteWithValidToken;
-      chai
-        .request(server)
-        .get('/getnotes')
-        .set({ authorization: token })
-        .end((err, res) => {
-          res.should.have.status(201);
-          done();
-        });
+        const token = noteDB.notes.getNoteWithValidToken;
+        chai
+            .request(server)
+            .get('/getnotes')
+            .set({ authorization: token })
+            .end((err, res) => {
+                res.should.have.status(201);
+                done();
+            });
     });
-  
+
     it('givenCreateNotes_whenInvalidToken_shouldNotbeGet', (done) => {
-      const token = noteDB.notes.getNoteWithInValidToken;
-      chai
-        .request(server)
-        .get('/getnotes')
-        .set({ authorization: token })
-        .end((err, res) => {
-          res.should.have.status(400);
-          done();
-        });
+        const token = noteDB.notes.getNoteWithInValidToken;
+        chai
+            .request(server)
+            .get('/getnotes')
+            .set({ authorization: token })
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            });
     });
-  });
+});
