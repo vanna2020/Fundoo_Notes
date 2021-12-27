b194f16e761d248fd0ee251c392e27f07db74b7a
vanna2020 <vannasingh1999@gmail.com>
2021-12-27 15:03:53 +0530

    Refactor: Updated valid entry of token  for AddLabelById By using TDD

--- Test/label.js
+++ Test/label.js
@@ -3,6 +3,7 @@ const chaiHttp = require('chai-http');
 const server = require('../server');
 const faker = require('faker');
 chai.use(chaiHttp);
+const labelDB = require('./label.json');
 const { expect } = require('chai');
 chai.should();
 
@@ -14,6 +15,17 @@ describe('Add label by id api ', () => {
             .post('/addlabel/:id')
             .end((err, res) => {
                 res.should.have.status(500);
+                done();
+            });
+    });
+    it.only('Given Token give true When Valid Entry of Token', (done) => {
+        const token = labelDB.label.validToken
+        chai
+            .request(server)
+            .post('/addlabel/:id')
+            .set({ authorization: token })
+            .end((err, res) => {
+                res.should.have.status(201);
                 done();
             });
     });
